7ec3ecace3223acf1c32191a9ac85a990c641381
the-jus <techdroid11112000@gmail.com>
2022-09-07 19:05:06 +0530

    Few changes added

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,11 +9,11 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Thejus T </span>
-            from <span className="purple"> Kozhikode,Kerala,India.</span>
-            <br />I am a CSE Undergraduate from MACE , Kothamangalam
+            from <span className="purple">India.</span>
+            <br />A enthusiastic Computer Science Engineer graduated from MACE , Kothamangalam
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
@@ -29,11 +29,6 @@ function AboutCard() {
               <ImPointRight /> Designing
             </li>
           </ul>
-
-          {/* <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer> */}
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -22,7 +22,7 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
+        {/* <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
             href={pdf}
@@ -32,7 +32,7 @@ function ResumeNew() {
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
-        </Row>
+        </Row> */}
 
         {/* <Row className="resume">
           <Document file={resumeLink} className="d-flex justify-content-center">
